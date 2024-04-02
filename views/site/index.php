e1b79b1c31a9fa2f55072217c295be685298983d
kaluginavioletta <129511496+kaluginavioletta@users.noreply.github.com>
2024-04-02 13:36:21 +0700

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -259,20 +259,3 @@
         });
     });
 </script>
-<?php foreach ($employees as $employee) { ?>
-    <div style="background-color: #2771ffa1; display: flex; flex-direction: column; width: 500px; margin-left: 50px; margin-bottom: 200px; position: relative; align-items: baseline; border-radius: 20px; border: 1px solid #2947ec;">
-        <div style="display: flex; background-color: #678dff; width: 300px; align-items: center; margin-top: 20px; margin-left: auto; margin-right: auto; text-align: center;">
-            <h3><?= $employee->surname ?></h3>&nbsp;
-            <h3><?= $employee->name ?></h3>&nbsp;
-            <h3><?= $employee->patronymic ?></h3>&nbsp;
-        </div>
-        <div style="margin-left: 20px">
-            <h4>Пол: <?= $employee->gender ?></h4>
-            <h4>Дата рождения: <?= $employee->dob ?></h4>
-            <h4>Адрес прописки: <?= $employee->address ?></h4>
-            <h4>Должность: <?= $employee->id_post->name ?></h4>
-            <h4>Прикрепление к подразделению: <?= $employee->id_unit->name ?></h4>
-            <img src="<?= $employee->img ?>" alt="<?= $employee->surname ?>" style="max-width: 100px; max-height: 100px;">
-        </div>
-    </div>
-<?php } ?>
