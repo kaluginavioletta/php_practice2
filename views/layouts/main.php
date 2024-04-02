f207f7325293131b1b399a83b8f9198b09e96a85
kaluginavioletta <129511496+kaluginavioletta@users.noreply.github.com>
2024-04-03 01:40:56 +0700

    Поиск, фильтрация по подразделениям и составу

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -11,23 +11,24 @@
 <header>
     <nav style="position: relative; display: flex; gap: 15px; align-items: center; justify-content: space-around; margin-bottom: 50px;">
         <?php
-            if (app()->auth::user()->id_role === 1) :
-                ?>
-                <a href="<?= app()->route->getUrl('/logout') ?>" style="color: white; text-decoration: none;">Выход (<?= app()->auth::user()->name ?>)</a>
-            <?php
-            elseif (app()->auth::user()->id_role === 2) :
-                ?>
-                <a href="<?= app()->route->getUrl('/') ?>" style="color: white; text-decoration: none;">Главная</a>
-                <a href="<?= app()->route->getUrl('/employee') ?>" style="color: white; text-decoration: none;">Добавить сотрудника</a>
-                <a href="<?= app()->route->getUrl('/unit') ?>" style="color: white; text-decoration: none;">Добавить подразделение</a>
-                <form method="get" action="<?= app()->route->getUrl('/search') ?>">
-                    <input type="text" name="query" placeholder="Поиск по ФИО">
-                    <button type="submit" style="-webkit-writing-mode: horizontal-tb !important; -webkit-appearance: button; border-color: rgb(216, 216, 216) rgb(209, 209, 209) rgb(186, 186, 186); border-style: solid; border-width: 1px; padding: 1px 7px 2px; text-rendering: auto; color: initial; display: inline-block; margin: 0em; font: 400 11px system-ui; border-radius: 15px; text-align: center; background-color: #3f39f7; width: 110px; height: 20px;"><a href="#" style="color: white; text-decoration: none;">Искать</button>
-                </form>
-                <a href="<?= app()->route->getUrl('/logout') ?>" style="color: white; text-decoration: none;">Выход (<?= app()->auth::user()->name ?>)</a>
-            <?php
-            endif;
+        if (app()->auth::user()->id_role === 1) :
             ?>
+            <a href="<?= app()->route->getUrl('/logout') ?>" style="color: white; text-decoration: none;">Выход (<?= app()->auth::user()->name ?>)</a>
+        <?php
+        elseif (app()->auth::user()->id_role === 2) :
+            ?>
+            <a href="<?= app()->route->getUrl('/') ?>" style="color: white; text-decoration: none;">Главная</a>
+            <a href="<?= app()->route->getUrl('/employee') ?>" style="color: white; text-decoration: none;">Добавить сотрудника</a>
+            <a href="<?= app()->route->getUrl('/unit') ?>" style="color: white; text-decoration: none;">Добавить подразделение</a>
+            <form method="GET" action="<?= app()->route->getUrl('/search') ?>">
+                <input name="csrf_token" type="hidden" value="<?= app()->auth::generateCSRF() ?>"/>
+                <input type="text" name="query" placeholder="Поиск по ФИО">
+                <button type="submit" style="-webkit-writing-mode: horizontal-tb !important; -webkit-appearance: button; border-color: rgb(216, 216, 216) rgb(209, 209, 209) rgb(186, 186, 186); border-style: solid; border-width: 1px; padding: 1px 7px 2px; text-rendering: auto; color: initial; display: inline-block; margin: 0em; font: 400 11px system-ui; border-radius: 15px; text-align: center; background-color: #3f39f7; width: 110px; height: 20px;"><a href="#" style="color: white; text-decoration: none;">Искать</button>
+            </form>
+            <a href="<?= app()->route->getUrl('/logout') ?>" style="color: white; text-decoration: none;">Выход (<?= app()->auth::user()->name ?>)</a>
+        <?php
+        endif;
+        ?>
     </nav>
 </header>
 <main style="margin-left: 100px;">
