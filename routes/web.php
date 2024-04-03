9cb8375e4d88e38eec1ddb25e1a0883112208eb9
kaluginavioletta <129511496+kaluginavioletta@users.noreply.github.com>
2024-04-03 17:19:50 +0700

    Редактирование путей

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use Src\Route;
+
 Route::add(['GET', 'POST'], '/', [Controller\Site::class, 'index'])->middleware('auth');
 Route::add(['GET', 'POST'], '/employee', [Controller\Site::class, 'employee'])->middleware('employee');
 Route::add(['GET', 'POST'], '/post', [Controller\Site::class, 'post'])->middleware('employee');
 Route::add(['GET', 'POST'], '/composition', [Controller\Site::class, 'composition'])->middleware('employee');
 Route::add(['GET', 'POST'], '/unit', [Controller\Site::class, 'unit'])->middleware('employee');
+Route::add(['GET', 'POST'], '/search', [Controller\Site::class, 'search'])->middleware('auth');
 Route::add(['GET', 'POST'], '/view', [Controller\Site::class, 'view'])->middleware('employee');
 Route::add(['GET', 'POST'], '/signup', [Controller\Site::class, 'signup'])->middleware('admin');
 Route::add(['GET', 'POST'], '/login', [Controller\Site::class, 'login']);
