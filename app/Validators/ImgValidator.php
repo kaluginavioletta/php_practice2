b6f04eeee3e17849528a945db74554dc67d2dff5
kaluginavioletta <129511496+kaluginavioletta@users.noreply.github.com>
2024-04-04 18:53:43 +0700

    Update ImgValidator.php

--- app/Validators/ImgValidator.php
+++ app/Validators/ImgValidator.php
@@ -6,17 +6,18 @@ use Src\Validator\AbstractValidator;
 
 class ImgValidator extends AbstractValidator
 {
+
+    protected string $message = 'Field :field does not match the format';
     public function rule(): bool
     {
-
-        if (isset($_FILES['img']) && !empty($_FILES['img']['name'])) {
+        if (isset($this->value['name']) && !empty($this->value['name'])) {
             $allowed_extensions = ['jpg', 'jpeg', 'png', 'gif'];
-            $file_extension = strtolower(pathinfo($_FILES['img']['name'], PATHINFO_EXTENSION));
+            $file_extension = strtolower(pathinfo($this->value['name'], PATHINFO_EXTENSION));
             if (!in_array($file_extension, $allowed_extensions)) {
                 $this->message = 'Не хороший формат для изображения';
                 return false;
             }
-            if ($_FILES['img']['size'] > 2 * 1024 * 1024) { // 2 MB
+            if ($this->value['size'] > 2 * 1024 * 1024) { // 2 MB
                 $this->message = 'Больше 2 Мб';
                 return false;
             }
